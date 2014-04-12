88c7e63046bd0a0c176f36f482780754e053ad6e
jsebfranck <jsebfranck@gmail.com>
2014-04-12 22:18:16 +0200

    Cleaned unused files and created lastMatch and calendar views

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,31 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
+var appControllers = angular.module('myApp.controllers', []);
 
-  }])
-  .controller('MyCtrl2', [function() {
+appControllers.controller('LastMatchController', function($scope) {
+  $scope.match = {
+    'homeTeam': 'Olympique Lyonnais',
+    'awayTeam': 'Paris SG',
+    'homeScore': 0,
+    'awayScore': 1,
+    'date': '2014-04-13'
+  };
+});
 
-  }]);
+appControllers.controller('CalendarController', function($scope) {
+  $scope.matches = [
+    {'homeTeam': 'Olympique Lyonnais',
+     'awayTeam': 'Paris SG',
+     'date': '2014-04-19'
+    },
+    {'homeTeam': 'Toulouse',
+     'awayTeam': 'Olympique Lyonnais',
+     'date': '2014-04-20'
+    },
+    {
+     'homeTeam': 'Olympique Lyonnais',
+     'awayTeam': 'Bastia',
+     'date': '2014-04-26'
+    }];
+});
